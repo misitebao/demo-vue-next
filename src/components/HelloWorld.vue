4b86c5dc329a5f161142561d9277f6a98b3a4dff
misitebao <i@misitebao.com>
2020-06-18 23:46:21 +0800

    测试ref

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,27 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p v-text="name"></p>
 </template>
 
 <script>
+import { reactive, ref } from "vue";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   },
   data() {
     return {
       count: 0
-    }
+    };
+  },
+  setup() {
+    let name = ref("zhangsan");
+
+    return {
+      name
+    };
   }
-}
+};
 </script>
